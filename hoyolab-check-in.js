7d7141c0b2ae6f92a759a2dd16ffed3bca3360fd
Kitipat Teacha <kitipat_teacha@outlook.co.th>
2023-05-16 02:46:28 +0700

    test

--- hoyolab-check-in.js
+++ hoyolab-check-in.js
@@ -1,35 +1,38 @@
 const axios = require("axios");
 
+const headers = {
+  Cookie: `ltoken=${process.env.LTOKEN}; ltuid=${process.env.LTUID};`,
+};
+const genshinCheckInURL =
+  "https://sg-hk4e-api.hoyolab.com/event/sol/sign?lang=en-us&act_id=e202102251931481";
+
+const starRailCheckInURL =
+  "https://sg-public-api.hoyolab.com/event/luna/os/sign?lang=en-us&act_id=e202303301540311";
+
 const randomDelayExecuteTime = () => {
-  // const MAX_THRESHOLD = 30 * 60 * 1000;
-  const MAX_THRESHOLD = 50000;
+  const MAX_THRESHOLD = 30 * 60 * 1000;
   const randomDelayExecuteTimeInMillisec = Math.random() * MAX_THRESHOLD;
-  console.log(randomDelayExecuteTimeInMillisec);
+  console.log(
+    `will execute in :${randomDelayExecuteTimeInMillisec / (1000 * 60)} minutes`
+  );
   return new Promise((resolve) =>
     setTimeout(resolve, randomDelayExecuteTimeInMillisec)
   );
 };
 
-const genshinCheckInURL =
-  "https://sg-hk4e-api.hoyolab.com/event/sol/sign?lang=en-us&act_id=e202102251931481";
-
-const run = async () => {
-  const headers = {
-    Cookie: `ltoken=${process.env.LTOKEN}; ltuid=${process.env.LTUID};`,
-  };
-
+const checkIn = async (url, game) => {
   await randomDelayExecuteTime();
 
-  console.log("test execute");
+  const response = await axios.post(url, null, {
+    headers,
+  });
 
-  // axios
-  //   .post(genshinCheckInURL, null, { headers })
-  //   .then((response) => {
-  //     console.log(response.data);
-  //   })
-  //   .catch((error) => {
-  //     console.log(error);
-  //   });
+  console.log(`${game} check-in result : ${response}`);
+};
+
+const run = async () => {
+  await checkIn(genshinCheckInURL, "Genshin Impact");
+  await checkIn(starRailCheckInURL, "Star Rail");
 };
 
 run();
