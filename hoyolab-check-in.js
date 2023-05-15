674f72a5e3b477de7b7dd424fdc448e4bb629f7f
Kitipat Teacha <kitipat_teacha@outlook.co.th>
2023-05-16 02:00:21 +0700

    fix

--- hoyolab-check-in.js
+++ hoyolab-check-in.js
@@ -1,18 +1,18 @@
-import { getInput, setOutput, setFailed } from "@actions/core";
-import { context } from "@actions/github";
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 async function run() {
   try {
     // `who-to-greet` input defined in action metadata file
-    const nameToGreet = getInput("who-to-greet");
+    const nameToGreet = core.getInput("who-to-greet");
     console.log(`Hello ${nameToGreet}!`);
     const time = new Date().toTimeString();
-    setOutput("time", time);
+    core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(context.payload, undefined, 2);
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
     console.log(`The event payload: ${payload}`);
   } catch (error) {
-    setFailed(error.message);
+    core.setFailed(error.message);
   }
 }
 
