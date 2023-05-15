6720bd42878a85e8de2d767fb3c8bb666337ca39
Kitipat Teacha <kitipat_teacha@outlook.co.th>
2023-05-16 02:05:40 +0700

    fix

--- hoyolab-check-in.js
+++ hoyolab-check-in.js
@@ -2,9 +2,7 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 async function run() {
-  try {
-    console.log('The secret is:', process.env.LTOKEN);
-  }
+  console.log("The secret is:", process.env.LTOKEN);
 }
 
 run();
