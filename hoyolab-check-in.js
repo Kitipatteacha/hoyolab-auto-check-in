56403e323c926d41493a6f87be57450310547563
Kitipat Teacha <kitipat_teacha@outlook.co.th>
2023-05-16 02:21:25 +0700

    add axios

--- hoyolab-check-in.js
+++ hoyolab-check-in.js
@@ -1,22 +1,17 @@
+const axios = require("axios");
+
 const genshinCheckInURL =
   "https://sg-hk4e-api.hoyolab.com/event/sol/sign?lang=en-us&act_id=e202102251931481";
 
 async function run() {
-  console.log("The secret is:", process.env.LTOKEN);
-
-  const header = {
+  const headers = {
     Cookie: `ltoken=${process.env.LTOKEN}; ltuid=${process.env.LTUID};`,
   };
 
-  const options = {
-    method: "POST",
-    headers: header,
-    muteHttpExceptions: true,
-  };
-
-  fetch(genshinCheckInURL, options)
+  axios
+    .post(genshinCheckInURL, null, { headers })
     .then((response) => {
-      console.log(response);
+      console.log(response.data);
     })
     .catch((error) => {
       console.log(error);
