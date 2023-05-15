f4a7942b0473a3aeb0a6968e2104ddc1e20cde33
Kitipat Teacha <kitipat_teacha@outlook.co.th>
2023-05-16 02:33:55 +0700

    test

--- hoyolab-check-in.js
+++ hoyolab-check-in.js
@@ -1,21 +1,35 @@
 const axios = require("axios");
 
+const randomDelayExecuteTime = () => {
+  // const MAX_THRESHOLD = 30 * 60 * 1000;
+  const MAX_THRESHOLD = 5000;
+  const randomDelayExecuteTimeInMillisec = Math.random() * MAX_THRESHOLD;
+  console.log(randomDelayExecuteTimeInMillisec);
+  return new Promise((resolve) =>
+    setTimeout(resolve, randomDelayExecuteTimeInMillisec)
+  );
+};
+
 const genshinCheckInURL =
   "https://sg-hk4e-api.hoyolab.com/event/sol/sign?lang=en-us&act_id=e202102251931481";
 
-async function run() {
+const run = async () => {
   const headers = {
     Cookie: `ltoken=${process.env.LTOKEN}; ltuid=${process.env.LTUID};`,
   };
 
-  axios
-    .post(genshinCheckInURL, null, { headers })
-    .then((response) => {
-      console.log(response.data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-}
+  await randomDelayExecuteTime();
+
+  console.log("test execute");
+
+  // axios
+  //   .post(genshinCheckInURL, null, { headers })
+  //   .then((response) => {
+  //     console.log(response.data);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
+};
 
 run();
