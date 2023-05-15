16958f9a2f41c93b2d80dc7372d7012cd428d681
Kitipat Teacha <kitipat_teacha@outlook.co.th>
2023-05-16 02:16:37 +0700

    add checkin scripts

--- hoyolab-check-in.js
+++ hoyolab-check-in.js
@@ -1,8 +1,26 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
+const genshinCheckInURL =
+  "https://sg-hk4e-api.hoyolab.com/event/sol/sign?lang=en-us&act_id=e202102251931481";
 
 async function run() {
   console.log("The secret is:", process.env.LTOKEN);
+
+  const header = {
+    Cookie: `ltoken=${process.env.LTOKEN}; ltuid=${process.env.LTUID};`,
+  };
+
+  const options = {
+    method: "POST",
+    headers: header,
+    muteHttpExceptions: true,
+  };
+
+  fetch(genshinCheckInURL, options)
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
 run();
