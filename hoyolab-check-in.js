4a4a5e5ad62f3a5eab7a120f4dce1182cb702c59
Kitipat Teacha <kitipat_teacha@outlook.co.th>
2023-05-16 01:58:54 +0700

    test

--- hoyolab-check-in.js
+++ hoyolab-check-in.js
@@ -1,5 +1,19 @@
+import { getInput, setOutput, setFailed } from "@actions/core";
+import { context } from "@actions/github";
+
 async function run() {
-  console.log("Hello World");
+  try {
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = getInput("who-to-greet");
+    console.log(`Hello ${nameToGreet}!`);
+    const time = new Date().toTimeString();
+    setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    setFailed(error.message);
+  }
 }
 
 run();
